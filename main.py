0c10a7a1ef2d78605ad70c133573dc1e732bf300
Belkacem <abel-and-co@hotmail.com>
2023-05-13 22:21:40 +0200

    Version initiale

---
+++ main.py
@@ -0,0 +1,59 @@
+# Ce code présente animation simple pour présenter la régression linéaire.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib.animation import FuncAnimation
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+x = []
+y = []
+
+def create_pseudo_random_data(x):
+    variation = np.random.randint(-6, 6)
+    y = 2 * x + 1 + variation
+    return y
+
+def animate(i):
+    # on place des points aléatoires mais continue en x
+    previous_x = x[-1] if len(x) > 0 else 0
+    x.append(previous_x + 1)
+    y.append(create_pseudo_random_data(x[-1]))
+    # on affiche les points (on ne relie pas les points)
+    ax1.clear()
+    ax1.scatter(x, y)
+    if len(x) > 2:
+        # on calcule la régression linéaire
+        moyenne_x = np.mean(x)
+        moyenne_y = np.mean(y)
+        moyenne_x_carre = np.mean([x_i * x_i for x_i in x])
+
+# "Ce code est un exemple simple d'animation pour présenter la régression linéaire. L'animation
+# consiste en la création de points aléatoires, suivis de l'ajustement d'une droite de régression
+# linéaire aux données.
+# Dans cette animation, nous avons une fonction nommée create_pseudo_random_data(x) qui
+# prend en entrée une valeur de x et retourne une valeur de y générée aléatoirement en utilisant
+# la relation y = 2 * x + 1 avec une variation aléatoire.
+# La fonction animate(i) est appelée par FuncAnimation à chaque intervalle de temps défini
+# (dans ce cas, toutes les 1000 millisecondes). À chaque appel de animate(i) , nous ajoutons un
+# nouveau point de données à notre ensemble de données, puis nous recalculons la droite de
+# régression linéaire en utilisant tous les points de donnée
+
+
+        moyenne_xy = np.mean([x_i * y_i for x_i, y_i in zip(x, y)])
+        # on calcule les coefficients de la droite
+        a = (moyenne_xy - moyenne_x * moyenne_y) / (moyenne_x_carre - moyenne_x * moyenne_x)
+        b = moyenne_y - a * moyenne_x
+        print('a =', a, 'b =', b)
+        # on affiche la droite
+        x_min = min(x)
+        x_max = max(x)
+        y_min = a * x_min + b
+        y_max = a * x_max + b
+        ax1.plot([x_min, x_max], [y_min, y_max], color='red')
+ani = FuncAnimation(fig, animate, interval=1000)
+plt.show()
